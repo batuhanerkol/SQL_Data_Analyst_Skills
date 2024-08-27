e7f4284c8f8765e1ec0624ce2cfd7d3c742082a9
Batuhan Erkol <erkolbatuhan1@gmail.com>
2024-08-27 17:19:49 +0300

    3_top_demanded_skills added

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -21,12 +21,37 @@ LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 
 WHERE
     job_title_short = 'Data Analyst' AND
-    job_location LIKE '%Anywhere%' AND
+    job_location = 'Anywhere' AND
     salary_year_avg IS NOT NULL 
 
 ORDER BY
     salary_year_avg DESC
 LIMIT 100
 
+/*
+Same Result For Turkey
+*/
+
+SELECT 
+    name AS company_name,
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+
+FROM
+    job_postings_fact
+
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE
+    job_title_short LIKE '%Data%Analyst%' AND
+    job_location LIKE '%Türkiye%' AND
+    salary_year_avg IS NOT NULL 
 
 
+ORDER BY
+    salary_year_avg DESC
+LIMIT 100
